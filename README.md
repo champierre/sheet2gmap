5c92274f724c74a61076a86f3b0ae573fc1a1473
Junya Ishihara <junya@champierre.com>
2020-02-01 01:41:30 +0900

    Add constants.js.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # sheets2gmap
 
-[Demo site](https://champierre.github.io/sheets2gmap/)
+[Demo site](https://champierre.github.io/sheet2gmap/)
---
+++ constants.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyDuGsDZbA_qhW_994yBcxGTwh7QssOqGEw';
+const SHEET_ID = '1pTt8WpIbBziz8OxOP2NTCyfBlMWeZ9zRiZ_7tTyHbU8';
+
